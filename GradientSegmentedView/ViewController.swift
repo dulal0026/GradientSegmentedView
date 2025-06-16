2a586f0e94a7a802f8be4d832cde9b5bab33ac03
dulalDP <115469610+dulalDP@users.noreply.github.com>
2025-06-17 04:35:55 +0600

    add segmented view on view controller

--- GradientSegmentedView/ViewController.swift
+++ GradientSegmentedView/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var segmentedControl: SegmentedView = {
+        let sView: SegmentedView = SegmentedView(frame: .zero)
+        sView.translatesAutoresizingMaskIntoConstraints = false
+        return sView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        self.view.addSubview(segmentedControl)
+        
+        segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
+        segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
+        segmentedControl.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        
+        segmentedControl.viewModel = .init(items: ["Daily", "Weekly", "Monthly"])
     }
-
 
 }
 
